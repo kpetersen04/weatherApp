9c05396e0de88a35dc1deb9e8184eefe78070f1a
Kirstin <kpetersen@elon.edu>
2023-09-23 14:48:07 -0400

    installation and intial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>The most reliable weather app in the WORLD!</h1>
+    </>
+  );
+}
+
+export default App;
